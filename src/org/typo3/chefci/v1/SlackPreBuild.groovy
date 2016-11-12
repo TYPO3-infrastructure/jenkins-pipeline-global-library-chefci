8f742eaf38c878306c1b8179d18463bf34c13354
Steffen Gebert <steffen.gebert@typo3.org>
2016-11-12 19:42:39 +0100

    [BUGFIX] JOB_BASE_NAME equals branch name

--- src/org/typo3/chefci/v1/SlackPreBuild.groovy
+++ src/org/typo3/chefci/v1/SlackPreBuild.groovy
@@ -6,7 +6,7 @@ package org.typo3.chefci.v1;
 def execute(){
     slackSend(
             message: """
-            Cookbook ${env.JOB_BASE_NAME} (${env.BRANCH_NAME}) build ${env.BUILD_NUMBER} *started*:
+            Cookbook ${env.JOB_NAME} build #${env.BUILD_NUMBER} *started*:
             Author: ${env.CHANGE_AUTHOR} / ${env.CHANGE_TITLE}
             Change: ${env.CHANGE_URL}
             Build:  ${env.BUILD_URL}
