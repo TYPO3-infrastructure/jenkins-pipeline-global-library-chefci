f0cfaba802914d5ea6d5bc906662f435124b5d95
Steffen Gebert <steffen.gebert@typo3.org>
2016-12-01 09:41:52 +0100

    Revert "[FEATURE] Archive test-kitchen logs"
    This reverts commit 8ffe86b50dc5e49793b7e69748f708d66684fc39.
    
    Would be only executed after successful pipeline execution.

--- src/org/typo3/chefci/v1/ArchiveArtifacts.groovy
+++ src/org/typo3/chefci/v1/ArchiveArtifacts.groovy
@@ -4,12 +4,9 @@ package org.typo3.chefci.v1;
 
 def archiveArtifacts(){
     def artifactName = "cookbooks-${env.BUILD_TAG}.tar.gz"
-    echo "Skipping to archive complete cookooks, as long as we have private data in some cookbooks... :-/"
+    echo "Skipping archive step, as long as we have private data in some cookbooks... :-/"
     // sh("berks package ${artifactName}")
     // archive(includes: artifactName)
-
-    echo "Archiving test-kitchen logs"
-    archive(includes: ".kitchen/logs/*")
 }
 
 def execute(){
