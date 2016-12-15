8ce145b9646d8352bdfc21b34aa2f458480eba51
Steffen Gebert <steffen.gebert@typo3.org>
2016-12-15 21:11:03 +0100

    don't care about slacke

--- src/org/typo3/chefci/v1/SlackPreBuild.groovy
+++ src/org/typo3/chefci/v1/SlackPreBuild.groovy
@@ -4,10 +4,14 @@ package org.typo3.chefci.v1;
 
 
 def execute(){
-    slackSend(
-            message: "${env.JOB_NAME} build #${env.BUILD_NUMBER} *started*:\n  ${env.BUILD_URL}",
-            failOnError: false
-    )
+    try {
+        slackSend(
+                message: "${env.JOB_NAME} build #${env.BUILD_NUMBER} *started*:\n  ${env.BUILD_URL}",
+                failOnError: false
+        )
+    } catch(e) {
+        echo "Slack failed, I don't care.."
+    }
 }
 
 return this;
