bf83eed0aebdddadf14afe6957eddcd3ef39b536
Steffen Gebert <steffen.gebert@typo3.org>
2016-12-15 21:30:27 +0100

    okay, write to .berkshelf

--- vars/chefdk.groovy
+++ vars/chefdk.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(body) {
-    withDockerContainer(image: 'chef/chefdk:latest', args: '--volume=/var/lib/jenkins/.chef/:/.chef/:ro') {
+    withDockerContainer(image: 'chef/chefdk:latest', args: '--volume=/var/lib/jenkins/.chef/:/.chef/:ro --volume=/var/lib/jenkins/.berkshelf/:/.berkshelf/:rw') {
         body()
     }
 }
