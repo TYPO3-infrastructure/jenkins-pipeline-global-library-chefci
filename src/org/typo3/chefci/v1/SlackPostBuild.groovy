85b4fe8e037befe0a520e678fd428b0f18e9f430
Steffen Gebert <steffen.gebert@typo3.org>
2016-12-27 19:09:31 +0100

    Again, ignore Slack failure

--- src/org/typo3/chefci/v1/SlackPostBuild.groovy
+++ src/org/typo3/chefci/v1/SlackPostBuild.groovy
@@ -5,15 +5,20 @@ package org.typo3.chefci.v1;
 
 def execute(){
     def status = buildResultIsStillGood() ? 'good' : 'danger'
-    slackSend(
-            message: "${env.JOB_NAME} build #${env.BUILD_NUMBER} *finished*:\n  ${env.BUILD_URL}",
-            color: status,
-            failOnError: false
-    )
+
+    try {
+        slackSend(
+                message: "${env.JOB_NAME} build #${env.BUILD_NUMBER} *finished*:\n  ${env.BUILD_URL}",
+                color: status,
+                failOnError: false
+        )
+    } catch(e) {
+        echo "Slack failed, I don't care.."
+    }
 }
 
 def buildResultIsStillGood(){
-	return currentBuild.result != 'FAILURE'
+    return currentBuild.result != 'FAILURE'
 }
 
 return this;
