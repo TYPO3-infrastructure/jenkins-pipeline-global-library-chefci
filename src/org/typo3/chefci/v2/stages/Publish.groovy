1eb472a1f1ffc6c201412f2fafee6ddb3271b0db
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-23 15:00:46 +0100

    more refactoring

--- src/org/typo3/chefci/v2/stages/Publish.groovy
+++ src/org/typo3/chefci/v2/stages/Publish.groovy
@@ -7,8 +7,11 @@ import org.typo3.chefci.helpers.JenkinsHelper
 
 class Publish extends AbstractStage {
 
+    JenkinsHelper jenkinsHelper
+
     Publish(Object script, String stageName) {
         super(script, stageName)
+        jenkinsHelper = new JenkinsHelper(script)
     }
 
     @Override
@@ -61,11 +64,40 @@ class Publish extends AbstractStage {
         }
     }
 
+    /**
+     * Asks the user whether the cookbook should be uploaded
+     */
     protected publish() {
-        def userInput = true
-        def didTimeout = false
-        def versionPart
 
+        def userInput = getInput()
+
+        def newVersion
+        // if we don't get any user response, we do nothing
+        if (userInput.response) {
+            script.node{
+                newVersion = bumpVersion(userInput.versionBump)
+                upload()
+            }
+            // set the build name to something meaningful, i.e.
+            // #1 - 1.2.3 (patch)
+            jenkinsHelper.annotateBuildName(" - ${newVersion} (${userInput.versionBump})")
+
+        } else {
+            script.echo "No cookbook upload was triggered within timeout"
+            jenkinsHelper.annotateBuildName("(no upload)")
+        }
+    }
+
+    /**
+     * Asks the user for his/her opinion, if we should bump the version number and then upload this.
+     *
+     *  this will return something like
+     *  [response: true, reason:user, submitter:johndoe, versionBump:patch]
+     *  [response: false, reason:timeout]
+     *
+     * @return Map response options
+     */
+    protected Map getInput() {
         // generate the input dialog for version bump.
         // contains the choices patch/minor/major.
         // timeouts out after specified time.
@@ -74,36 +106,42 @@ class Publish extends AbstractStage {
         def timeoutOptions = [time: 15, unit: 'SECONDS']
 
         // call the input dialog
-        // this will return something like
-        // [response: true, reason:user, submitter:johndoe, versionBump:patch]
-        // [response: false, reason:timeout]
-        Map input = inputWithTimeout([inputOptions: inputOptions, timeoutOptions: timeoutOptions])
+        Map inputValues = inputWithTimeout([inputOptions: inputOptions, timeoutOptions: timeoutOptions])
 
+        script.echo "Got input ${inputValues}"
 
-        script.echo "Got input ${input}"
-
-        script.node {
-            def jenkinsHelper = new JenkinsHelper(script)
-
-            // if we don't get any user response, we do nothing
-            if (input.response) {
-                // TODO get rid of `bundle install`
-                script.sh 'chef exec bundle install'
-                // TODO make thor-scmversion globally accessible and get rid of `Thorfile`
-                // TODO see http://stackoverflow.com/questions/41474735/use-global-thorfile/41474996
-                script.sh "chef exec thor version:bump ${input.versionBump}"
-                def newVersion = script.readFile('VERSION')
-                // TODO enable Jenkins to push to Github
-                // sh("git push origin ${newVersion}")
-                // TODO remove comment once we've finished this...
-                //script.sh("berks upload")
-                script.echo "Could upload now"
-                jenkinsHelper.annotateBuildName(" - ${newVersion} (${input.versionBump})")
-            } else {
-                jenkinsHelper.annotateBuildName("(no upload)")
-                script.echo "No cookbook upload was triggered within timeout"
-            }
-        }
+        inputValues
     }
 
+    /**
+     * Increases the version number by level {@code level} using thor-scmversion and pushes git tag.
+     *
+     * @param level patch/minor/major
+     * @return new version number
+     */
+    protected String bumpVersion(String level) {
+        // TODO get rid of `bundle install`
+        script.sh 'chef exec bundle install'
+        // TODO make thor-scmversion globally accessible and get rid of `Thorfile`
+        // TODO see http://stackoverflow.com/questions/41474735/use-global-thorfile/41474996
+        script.sh "chef exec thor version:bump ${level}"
+        def newVersion = script.readFile('VERSION')
+        // TODO enable Jenkins to push to Github
+        // sh("git push origin ${newVersion}")
+
+        newVersion
+    }
+
+
+    /**
+     * Uploads the cookbook
+     *
+     * @param userInput
+     * @return
+     */
+    protected upload() {
+        // TODO remove comment once we've finished this...
+        //script.sh("berks upload")
+        script.echo "Could upload now"
+    }
 }
